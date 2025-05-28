2c9588fbcc6d2b3968309ffe8d893574f4b3514c
varchasvee <varchasvee108@gmail.com>
2025-05-28 15:21:10 +0530

    skeleton added

--- src/app/(protected)/video/[renderId]/_components/video-view.tsx
+++ src/app/(protected)/video/[renderId]/_components/video-view.tsx
@@ -1,12 +1,34 @@
 "use client";
 
-import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect, useState } from "react";
 
 interface VideoViewProps {
   videoUrl: string;
 }
 
-const VideoView: React.FC<VideoViewProps> = ({ videoUrl }) => {
+const VideoSkeleton: React.FC = () => {
+  return (
+    <div className="w-full h-full flex flex-col gap-4 p-4">
+      <Skeleton className="w-full h-8" />
+      <Skeleton className="w-full h-[calc(100%-2rem)]" />
+    </div>
+  );
+};
+
+const VideoView = ({ videoUrl }: VideoViewProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (videoUrl) {
+      setIsLoading(false);
+    }
+  }, [videoUrl]);
+
+  if (isLoading) {
+    return <VideoSkeleton />;
+  }
+
   return (
     <div className="w-full h-full">
       <video src={videoUrl} controls className="w-full h-full object-contain" />
